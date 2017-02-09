5988867a95d1fc50bdc8767c7c4cda37b937ae51
sprakash <satyap20@yahoo.com>
2017-02-09 14:53:07 -0500

    Changed the HTML

---
+++ pythonanywhere-wsgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+path = '/home/satyaprakash20/my-first-blog'  # use your own PythonAnywhere username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
